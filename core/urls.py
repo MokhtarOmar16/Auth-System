f2f630131601c5f96248c1d1128c759609142eb9
Mokhtar Omar <AlMokhtarOmar24@gmail.com>
2024-09-09 05:49:58 +0300

    adding refresh endpoint to router

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .apis.views import registerview, loginview
+from .apis.views import registerview, loginview, refreshview
 urlpatterns = [
     path("register/", registerview.RegisterView.as_view(), name="register"),
-    path("login/", loginview.LoginView.as_view(), name="login")
+    path("login/", loginview.LoginView.as_view(), name="login"),
+    path("refresh/", refreshview.RefreshView.as_view(), name="refresh")
 ]
